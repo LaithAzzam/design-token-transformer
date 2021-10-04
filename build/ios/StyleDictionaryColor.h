aeb6d567ba4990dea153ab1cb32b8c1323eb9a11
Automated Publisher <actions@users.noreply.github.com>
2021-10-04 07:53:13 +0000

    Automated publish: Mon Oct  4 07:53:13 UTC 2021 daa8512a5fe8dd7a474e20a261eb747306e7debb

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,38 +4,20 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 04 Oct 2021 07:50:30 GMT
+// Generated on Mon, 04 Oct 2021 07:53:12 GMT
 
 
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-BordersSingleStroke,
-BordersDashedOutsideStroke,
-BordersSingleStyleStroke,
-GradientGradientSingleWithMultipleColorStopsStops0Color,
-GradientGradientSingleWithMultipleColorStopsStops1Color,
-GradientGradientSingleWithMultipleColorStopsStops2Color,
-GradientGradientSingleWithMultipleColorStopsStops3Color,
-GradientGradientMultiple0Stops0Color,
-GradientGradientMultiple0Stops1Color,
-GradientGradientMultiple1Stops0Color,
-GradientGradientMultiple1Stops1Color,
-GradientGradientMultiple2Stops0Color,
-GradientGradientMultiple2Stops1Color,
-GradientGradientMultiple3Stops0Color,
-GradientGradientMultiple3Stops1Color,
+ColorNotExported,
 ColorColorsMultipleFills0,
 ColorColorsMultipleFills1,
 ColorColorsSingleBlue,
 ColorColorsSpecialCharacters,
 ColorColorsSpecialCharactersNderung,
 ColorLightBackground,
-ColorDarkBackground,
-EffectDropShadowSingleColor,
-EffectInnerShadowMultiple0Color,
-EffectInnerShadowMultiple1Color,
-EffectInnerShadowMultiple2Color
+ColorDarkBackground
 };
 
 @interface StyleDictionaryColor : NSObject

6bf7878ba5400175c68cc9b5d6ad87c818a89952
Automated Publisher <actions@users.noreply.github.com>
2021-10-07 21:21:32 +0000

    Automated publish: Thu Oct  7 21:21:32 UTC 2021 1bdddcdf95947ddc4b3c50fd7f9a610874d31c91

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,18 +4,30 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 04 Oct 2021 21:08:42 GMT
+// Generated on Thu, 07 Oct 2021 21:21:31 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let accent = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let darkBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let info = UIColor(red: 0.000, green: 0.400, blue: 1.000, alpha: 1)
-    public static let lightBackground = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
-    public static let primary = UIColor(red: 0.988, green: 0.812, blue: 0.173, alpha: 1)
-    public static let success = UIColor(red: 0.000, green: 1.000, blue: 0.278, alpha: 1)
-    public static let warning = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let white = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let accentAccent = UIColor(red: 0.000, green: 0.576, blue: 0.839, alpha: 1)
+    public static let grayscaleBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let grayscaleGray400 = UIColor(red: 0.286, green: 0.318, blue: 0.380, alpha: 1)
+    public static let grayscaleGray500 = UIColor(red: 0.463, green: 0.490, blue: 0.537, alpha: 1)
+    public static let grayscaleGray600 = UIColor(red: 0.886, green: 0.886, blue: 0.886, alpha: 1)
+    public static let grayscaleGray700 = UIColor(red: 0.957, green: 0.957, blue: 0.961, alpha: 1)
+    public static let grayscaleGray800 = UIColor(red: 0.980, green: 0.984, blue: 0.984, alpha: 1)
+    public static let grayscaleGrey100 = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1)
+    public static let grayscaleGrey200 = UIColor(red: 0.157, green: 0.157, blue: 0.176, alpha: 1)
+    public static let grayscaleGrey300 = UIColor(red: 0.204, green: 0.204, blue: 0.235, alpha: 1)
+    public static let grayscaleWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let primaryInteractivity = UIColor(red: 0.000, green: 0.314, blue: 0.675, alpha: 1)
+    public static let primaryShade1 = UIColor(red: 0.000, green: 0.263, blue: 0.463, alpha: 1)
+    public static let primaryShade2 = UIColor(red: 0.906, green: 0.953, blue: 0.996, alpha: 1)
+    public static let primaryShade3 = UIColor(red: 0.388, green: 0.565, blue: 0.737, alpha: 1)
+    public static let secondarySecondary1 = UIColor(red: 0.549, green: 0.839, blue: 0.827, alpha: 1)
+    public static let secondarySecondary2 = UIColor(red: 0.933, green: 0.820, blue: 0.792, alpha: 1)
+    public static let supportAlert = UIColor(red: 0.965, green: 0.788, blue: 0.369, alpha: 1)
+    public static let supportError = UIColor(red: 0.953, green: 0.345, blue: 0.361, alpha: 1)
+    public static let supportSuccess = UIColor(red: 0.000, green: 0.522, blue: 0.337, alpha: 1)
 }
--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,20 +4,32 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 04 Oct 2021 21:08:42 GMT
+// Generated on Thu, 07 Oct 2021 21:21:31 GMT
 
 
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-ColorPrimary,
-ColorAccent,
-ColorWhite,
-ColorWarning,
-ColorSuccess,
-ColorInfo,
-ColorLightBackground,
-ColorDarkBackground
+ColorGrayscaleGray400,
+ColorGrayscaleBlack,
+ColorGrayscaleGray500,
+ColorGrayscaleGray600,
+ColorGrayscaleGray700,
+ColorGrayscaleGray800,
+ColorGrayscaleWhite,
+ColorGrayscaleGrey100,
+ColorGrayscaleGrey200,
+ColorGrayscaleGrey300,
+ColorSupportSuccess,
+ColorSupportError,
+ColorSupportAlert,
+ColorAccentAccent,
+ColorPrimaryInteractivity,
+ColorPrimaryShade1,
+ColorPrimaryShade2,
+ColorPrimaryShade3,
+ColorSecondarySecondary1,
+ColorSecondarySecondary2
 };
 
 @interface StyleDictionaryColor : NSObject
